03be22d6756fec74e7805a82498289d735d8b301
Yury <ydelendik@mozilla.com>
2013-07-22 10:25:44 -0500

    Using device orientation to change the photos

--- photos/photos.js
+++ photos/photos.js
@@ -1,4 +1,4 @@
-// Demo of the localStorage, FileReader
+// Demo of the localStorage, FileReader, deviceorientation
 function initPhotos() {
   // saving photos information in the local storage
   // https://developer.mozilla.org/en-US/docs/Web/Guide/DOM/Storage
@@ -118,7 +118,27 @@ function initPhotos() {
   // .. and some interesting stuff starts here
 
   function initShowView() {
-    // TODO add orientation change events
+    function click() {
+      photos.removeEventListener('click', click);
+      window.removeEventListener('deviceorientation', orientationChanged);
+      setPanel('main');
+    }
+
+    var view = storage.loadView(currentViewId);
+    var photos = document.getElementById('photos');
+    photos.textContent = '';
+    for (var i = 0; i < view.photos.length; i++) {
+      var photo = document.createElement('div');
+      photo.className = 'photo';
+      photo.dataset.orientation = view.photos[i].orientation;
+      var img = document.createElement('img');
+      img.src = view.photos[i].src;
+      photo.appendChild(img);
+      photos.appendChild(photo);
+    }
+
+    photos.addEventListener('click', click);
+    window.addEventListener('deviceorientation', orientationChanged);
   }
 
   function addView() {
@@ -197,6 +217,42 @@ function initPhotos() {
     var fileInput = document.getElementById('newitem');
     fileInput.addEventListener('change', inputChanged);
     fileInput.click();
+  }
+
+  function sigmoid(x) {
+    return x / Math.sqrt(1 + x * x);
+  }
+
+  function orientationChanged(e) {
+    // https://developer.mozilla.org/en-US/docs/WebAPI/Detecting_device_orientation
+    var NEXT_PHOTO_STEP = 20;
+    var EFFECT_SPEED = 6;
+
+    var photos = document.getElementById('photos');
+    var count = photos.children.length;
+
+    // tracking changes on both axes
+    var gamma = e.gamma + e.beta;
+
+    var ratio = ((gamma % NEXT_PHOTO_STEP) + NEXT_PHOTO_STEP) % NEXT_PHOTO_STEP;
+    var itemIndex = (Math.floor(Math.round(gamma - ratio) / NEXT_PHOTO_STEP) % count + count) % count;
+    ratio /= NEXT_PHOTO_STEP;
+    var nextItemIndex = (itemIndex + 1) % count;
+    var opacity = sigmoid((ratio - 0.5) * EFFECT_SPEED) / 2 + 0.5;
+
+    for (var i = 0; i < count; i++) {
+      var photo = photos.children[i];
+      if (i === itemIndex) {
+        photo.style.zIndex = 100;
+        photo.style.opacity = 1.0;
+      } else if (i === nextItemIndex) {
+        photo.style.zIndex = 101;
+        photo.style.opacity = opacity;
+      } else {
+        photo.style.zIndex = 0;
+        photo.style.opacity = 0;
+      }
+    }
   }
 
 
